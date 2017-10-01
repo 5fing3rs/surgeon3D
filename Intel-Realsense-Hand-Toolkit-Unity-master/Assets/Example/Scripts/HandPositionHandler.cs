d998c3a3f19394a6ca8cdc100984cbd59464ecb3
Saksham Gupta <shucon01@gmail.com>
2017-10-01 23:32:01 +0530

    Initial Commit

---
+++ Intel-Realsense-Hand-Toolkit-Unity-master/Assets/Example/Scripts/HandPositionHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Example
+{
+    public class HandPositionHandler : MonoBehaviour
+    {
+
+        public GameObject leftCursor, rightCursor;
+
+        void Start()
+        {
+            GameObject.Find("DepthCameraManager").GetComponent<HandPositionManager>().AddSubscriber(gameObject);
+        }
+
+        void OnLeftHandChange(Vector3 handPosition)
+        {
+            leftCursor.transform.position = Camera.main.transform.position + new Vector3(handPosition.x * 100, handPosition.y * 100, handPosition.z * 100) + Camera.main.transform.forward;
+        }
+
+        void OnRightHandChange(Vector3 handPosition)
+        {
+            rightCursor.transform.position = Camera.main.transform.position + new Vector3(handPosition.x * 100, handPosition.y * 100, handPosition.z * 100) + Camera.main.transform.forward;
+        }
+
+        void OnLeftHandAppear()
+        {
+            leftCursor.SetActive(true);
+        }
+
+        void OnLeftHandDisappear()
+        {
+            leftCursor.SetActive(false);
+        }
+
+        void OnRightHandAppear()
+        {
+            rightCursor.SetActive(true);
+        }
+
+        void OnRightHandDisappear()
+        {
+            rightCursor.SetActive(false);
+        }
+
+    }
+
+}
---
+++ Intel-Realsense-Hand-Toolkit-Unity-master/Assets/IRToolkit/Scripts/GesturalManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Intel.RealSense;
+using Intel.RealSense.Hand;
+
+public class GesturalManager : Publisher {
+
+	public Text gestureText;
+	public Text debugText;
+	public bool isEnableAllGesture;
+    public string[] gestureActions;
+
+    HandManager handManager;
+
+    string TAG = "Gestural Manager : ";
+
+	// Update is called once per frame
+	void Update () {
+        UpdateHandGesture();
+    }
+
+    // Enable Gesture
+    public void EnableGestures()
+    {
+        handManager = GetComponent<HandManager>();
+        if (isEnableAllGesture)
+        {
+            Debug.Log(TAG + "All Gesture Enabled!");
+            handManager.handConfiguration.EnableAllGestures();
+        }else
+        {
+            foreach (string gesture in gestureActions)
+            {
+                Debug.Log(TAG + gesture + " Enabled!");
+                handManager.handConfiguration.EnableGesture(gesture, true);
+            }
+        }
+        handManager.handConfiguration.ApplyChanges();
+        GetComponent<DepthCameraManger>().StartDevice();
+    }
+
+    // Update Gesture
+    public void UpdateHandGesture()
+    {
+        if (GetComponent<DepthCameraManger>().isStart)
+            if (handManager.handData != null)
+                if (handManager.handData.FiredGestureData != null)
+                {
+                    foreach (GestureData gesture in handManager.handData.FiredGestureData)
+                    {
+                        string camelCaseGestureName = CreateCamelCase(gesture.name);
+						debugText.text =gesture.ToString();
+						if (GetHandSide(gesture).ToString() == "BODY_SIDE_LEFT")
+                        {
+                            gestureText.text = "Left Hand " + camelCaseGestureName;
+                        }
+                        else if (GetHandSide(gesture).ToString() == "BODY_SIDE_RIGHT")
+                        {
+                            gestureText.text = "Right Hand" + camelCaseGestureName;
+						}
+
+                        gestureText.text = "Hand " + camelCaseGestureName;
+                        Boardcast("OnGesture", gesture);
+                    }
+                }
+    }
+
+    // Create Camel Case String
+    string CreateCamelCase(string normalString)
+    {
+        string[] arrStr = normalString.Split('_');
+        for (int i = 0;i<arrStr.Length;i++)
+            arrStr[i] = (arrStr[i][0] + "").ToUpper() + arrStr[i].Substring(1);
+        return string.Join("", arrStr);
+    }
+
+    // Get Hand Side from gesture
+    public BodySideType GetHandSide(GestureData gesture)
+    {
+        IHand hand;
+        if (GetComponent<HandManager>().handData.QueryHandDataById(gesture.handId, out hand) == Status.STATUS_NO_ERROR)
+        {
+            return hand.BodySide;
+        }
+        return BodySideType.BODY_SIDE_UNKNOWN;
+    }
+
+}
