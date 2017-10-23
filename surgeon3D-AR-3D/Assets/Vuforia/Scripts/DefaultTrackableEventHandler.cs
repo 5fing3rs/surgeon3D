b202be0317f287d10bacfea0402c8a595f71f817
pulle sravya <psssravya@gmail.com>
2017-10-23 14:03:30 +0530

    Velocity calculation for multiple target objects

--- surgeon3D-AR-3D/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ surgeon3D-AR-3D/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -17,7 +17,11 @@ namespace Vuforia
         #region PRIVATE_MEMBER_VARIABLES
 
         public TrackableBehaviour mTrackableBehaviour;
-
+        public int flag;
+        private Vector3 velocity = new Vector3(0, 0, 0);
+        private Vector3 previous = new Vector3(0, 0, 0);
+        private float time;
+        private float v;
         #endregion // PRIVATE_MEMBER_VARIABLES
 
 
@@ -52,10 +56,12 @@ namespace Vuforia
                 newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
             {
                 OnTrackingFound();
+                flag = 1;
             }
             else
             {
                 OnTrackingLost();
+                flag = 0;
             }
         }
 
@@ -108,6 +114,59 @@ namespace Vuforia
             //mTrackableBehaviour.transform()
         }
 
+        void OnGUI()
+        {
+            if(mTrackableBehaviour.TrackableName == "calculator")
+                GUI.Label(new Rect(Screen.width - 200, 0, 200, Screen.height - 40), "Velociy of " + mTrackableBehaviour.TrackableName + " is "+ v );
+            else if(mTrackableBehaviour.TrackableName == "remote")
+                GUI.Label(new Rect(Screen.width - 400, 50, 100, Screen.height - 80), "Velociy of " + mTrackableBehaviour.TrackableName + " is " + v);
+
+        }
+
+        void Update()
+        {
+            if (flag == 1)
+            {
+                if (time < 1)
+                    time += Time.deltaTime;
+
+                // We define a point in the target local reference 
+                // we take the bottom-left corner of the target, 
+                // just as an example
+                // Note: the target reference plane in Unity is X-Z, 
+                // while Y is the normal direction to the target plane
+                // time = Time.deltaTime;
+                else
+                {
+                    time = 0;
+                    Vector3 pointOnTarget = new Vector3(0.5f, 0, 0.5f);
+
+                    // We convert the local point to world coordinates
+                    Vector3 targetPointInWorldRef = mTrackableBehaviour.transform.TransformPoint(pointOnTarget);
+
+
+                    // We project the world coordinates to screen coords (pixels)
+                    Vector3 screenPoint = Camera.main.WorldToScreenPoint(targetPointInWorldRef);
+
+                    velocity.x = (float)(screenPoint.x - previous.x) / 1;
+                    velocity.y = (float)(screenPoint.y - previous.y) / 1;
+                    velocity.z = (float)(screenPoint.z - previous.z) / 1;
+                    // float v = Mathf.Sqrt(velocity.x*velocity.x+)
+                    v = velocity.magnitude;
+
+
+                    Debug.Log("target point in screen coords of : " + mTrackableBehaviour.TrackableName + screenPoint);
+                    Debug.Log("Distance traversed by  " + mTrackableBehaviour.TrackableName+ " is  "  + (screenPoint - previous));
+                    Debug.Log("velocity of  " + mTrackableBehaviour.TrackableName  + " is "+ v);
+                    previous = screenPoint;
+                }
+            }
+            else
+            {
+                 Debug.Log(mTrackableBehaviour.TrackableName + "Not being tracked");
+            }
+        }
+    }
+
         #endregion // PRIVATE_METHODS
     }
-}
